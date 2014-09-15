603dae03584572ea27bec8d272f1ed590b92a6bd
Ciro Santilli <ciro.santilli@gmail.com>
2014-09-15 09:00:07 +0200

    0

---
+++ multiline.rb
@@ -0,0 +1,37 @@
+# Multi multi add.
+a = 0
+b = 0
+c = 0
+
+f = 0
+g = 0
+h = 0
+
+# One multi add.
+a = 0
+b = 0
+c = 0
+
+f = 0
+g = 0
+h = 0
+
+# Multi multi modify.
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+g = 0
+h = 0
+
+# One multi modify.
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+g = 0
+h = 0
